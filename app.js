0239a321f930f6dd938311c1a93d1ef06b07779d
odisaputra <saputraodi393@gmail.com>
2022-08-15 14:32:58 +0700

    commit

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
-const app = require("express")();
-const PORT = Process.env.PORT  || 3000;
+const express = require('express');
+const config = require('config');
+const app = express();
+const port = config.get('server.port');
+const host = config.get('server.host');
 
-app.get("",(req, res)=> {
-    res.send("login.html");
+app.get('/', (req, res) => {
+    res.send('Hello World');
 });
-
-app.listen(PORT, ()=>{
-    console.log('app up at port ${PORT}');
-}); 
+const server = app.listen(port, host, (err) => {
+    if (err) {
+        console.log(err);
+        process.exit(1);
+    }
+    console.log(`Server is running on ${host}:${server.address().port}`);
+});
